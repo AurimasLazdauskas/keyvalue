720873d5a74290b30a587e2bb03a204adbde4b75
AurimasLazdauskas <aur.lazdauskas@gmail.com>
2024-01-23 21:45:07 +0200

    Add: test for KeyValueStore.Persist

--- store_test.go
+++ store_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"testing"
+)
 
 func TestKeyValueStoreInsertAndGet(t *testing.T) {
 	key := "one"
@@ -14,5 +19,36 @@ func TestKeyValueStoreInsertAndGet(t *testing.T) {
 
 	if result != value {
 		t.Errorf(key+" should output "+value+" but got: ", result)
+	}
+}
+
+func TestKeyValueStorePersist(t *testing.T) {
+	key := "one"
+	value := "1"
+
+	store := NewKeyValueStore()
+
+	store.Insert(key, value)
+
+	store.Persist()
+
+	file, err := os.Open("keyvalue.db")
+
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Scan()
+	dbFileLine := scanner.Text()
+
+	expectedResult := key + ":" + value
+
+	if dbFileLine != expectedResult {
+		t.Errorf("Should output "+expectedResult+" but got: ", dbFileLine)
 	}
 }
